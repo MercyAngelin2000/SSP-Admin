04ed00d1f52ae6923fb94f9d2c762808e35fcb69
MercyAngelin2000 <mercyangelin2000@gmail.com>
2024-06-04 18:36:29 +0530

    region user completed

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,9 +1,82 @@
-import React from 'react'
+import React from 'react';
+import './Login.css';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
 
 function Login() {
+
+  const { register, formState: { errors }, handleSubmit } = useForm();
+  let base_url = process.env.REACT_APP_BASE_URL
+  const login = (data) => {
+    const formData = new FormData();
+    formData.append('username', data.username);
+    formData.append('password', data.password);
+    axios.post(`${base_url}/users/login/`, formData)
+      .then((response) => {
+        if (response.data.status) {
+          localStorage.setItem('access-token', response.data.access_token);
+          window.location.href = '/dashboard';
+        } else {
+          alert(response.data.message);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
-    <div>Login</div>
-  )
+    <div className="main">
+      <div className="container1">
+        <center>
+          <div className="middle">
+            <div id="login">
+              <form onSubmit={handleSubmit(login)}>
+                <fieldset className="clearfix">
+                  <p className='mb-4'>
+                    <span className="fa fa-user">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                      </svg>
+                    </span>
+                    <input type="text" style={errors.username ? { border: '2px solid red' } : {}} placeholder="Username" {...register('username', { required: true })} />
+                    {errors.username && <span style={{ color: 'red', fontSize: 'small' }}>Username field is required</span>}
+                  </p>
+                  <p className='mb-4'>
+                    <span className="fa fa-lock">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lock-fill" viewBox="0 0 16 16">
+                        <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2m3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2" />
+                      </svg>
+                    </span>
+                    <input type="password" placeholder="Password" {...register('password', { required: true })} />
+                    {errors.password && <span style={{ color: 'red', fontSize: 'small' }}>Password field is required</span>}
+                  </p>
+
+                  <div>
+                    <span style={{ width: '48%', textAlign: 'left', display: 'inline-block' }}>
+                      <a className="" href="#">
+                        Forgot password?
+                      </a>
+                    </span>
+                    <span style={{ width: '50%', textAlign: 'right', display: 'inline-block' }}>
+                      <input type="submit" value="Sign In" />
+                    </span>
+                  </div>
+                </fieldset>
+                <div className="clearfix"></div>
+              </form>
+              <div className="clearfix"></div>
+            </div>
+            <div className="logo">
+              LOGO
+              <div className="clearfix"></div>
+            </div>
+          </div>
+        </center>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- src/views/RegionUser.js
+++ src/views/RegionUser.js
@@ -5,6 +5,7 @@ import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import axios from 'axios';
+import Swal from 'sweetalert2';
 
 const customStyles = {
     headCells: {
@@ -32,16 +33,30 @@ const schema = yup.object().shape({
         .required('Confirm password is required'),
     role: yup.string().required('Role is required'),
 });
+const schema1 = yup.object().shape({
+    name: yup.string().required('Name is required'),
+    username: yup.string().required('Username is required'),
+    role: yup.string().required('Role is required'),
+});
 function User() {
+    let base_url = process.env.REACT_APP_BASE_URL
+    var token = localStorage.getItem("access-token");
+    const [title, setTile] = useState("Add User")
     const { register, handleSubmit, formState: { errors }, reset } = useForm({
-        resolver: yupResolver(schema),
+        resolver: yupResolver(title === "Add User" ? schema : schema1),
     });
-    const [userdata,setUserData]=useState()
+    const [userdata, setUserData] = useState()
+    const [roleData, setRoleData] = useState()
+    const [editData, setEditData] = useState()
+    //pagination
+    const [skip, setSkip] = useState(0);
+    const [limit, setLimit] = useState(10);
+    const [total, setTotal] = useState(0);
     const columns = [
         {
             name: '#',
-            selector: (row,index) => index+1,
-            cell: (row,index) => <div>{index+1}</div>,
+            selector: (row, index) => index + 1,
+            cell: (row, index) => <div>{index + 1}</div>,
             sortable: true
         },
         {
@@ -60,56 +75,268 @@ function User() {
             name: 'Active Status',
             selector: row => row.active,
             cell: row => <div className="form-check form-switch">
-                <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" defaultChecked={row.active} onChange={handleActiveStatus} />
-                <label className="form-check-label" htmlFor="flexSwitchCheckChecked">{row?.active ? "Active" : "Inactive"}</label>
+                <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" defaultChecked={row.active} onChange={(event) => handleActiveStatus(event, row)} />
+                <label className="form-check-label" htmlFor="flexSwitchCheckChecked"></label>
             </div>,
             sortable: true
         },
+        {
+            name: 'Actions',
+            selector: row => row.status,
+            cell: row =>
+                <div>
+                    <button className='btn text-primary btn-sm me-2' data-bs-toggle="modal" data-bs-target="#staticBackdrop" onClick={() => handleEdit(row)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                        </svg>
+                    </button>
+                    <button className='btn text-danger btn-sm' onClick={() => handleDelete(row)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
+                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
+                        </svg>
+                    </button>
+                </div>,
+        },
     ];
-
+    const handleEdit = (row) => {
+        setTile("Edit User")
+        setEditData(row)
+        fetchRoleData()
+        reset({
+            name: row?.name,
+            username: row?.username,
+            role: row?.role_id,
+        })
+    }
+    const handleDelete = (row) => {
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+                confirmButton: "btn btn-success btn-sm",
+                cancelButton: "btn btn-danger me-2 btn-sm"
+            },
+            buttonsStyling: false
+        });
+        swalWithBootstrapButtons.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this user!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Yes, delete it!",
+            cancelButtonText: "No, cancel!",
+            reverseButtons: true,
+            heightAuto: false,
+            width: 400
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axios({
+                    method: 'delete',
+                    url: `${base_url}/users/${row?.id}`,
+                    headers: {
+                        "Authorization": "Bearer " + token
+                    }
+                }).then((response) => {
+                    let data = response?.data
+                    if (data?.status) {
+                        fetchUserListData()
+                        Swal.fire({
+                            toast: true,
+                            position: "center",
+                            icon: "success",
+                            title: "User deleted successfully",
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+                    }
+                    else {
+                        Swal.fire({
+                            toast: true,
+                            icon: "error",
+                            title: "Oops...",
+                            text: data?.message,
+                        });
+                    }
+                }).catch((error) => {
+                    console.log(error)
+                })
+            }
+        });
+    }
     const fetchUserListData = () => {
         axios({
             method: 'get',
-            url: 'http://192.168.1.148:8080/users/users/?user_type=region',
+            url: `${base_url}/users/users/?user_type=region&skip=${skip}&limit=${limit}`,
             headers: {
-                "Authorization": "Bearer "+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQWRtaW4iLCJ1c2VyX2lkIjoxLCJyb2xlX2lkIjoxLCJleHAiOjE3MTc0OTQ0Njl9.Vg-jyNA9joRsBucVGENHxexzLxTVKYJL9RYHjOVRkNY"
+                "Authorization": "Bearer " + token
             }
         }).then((response) => {
-            console.log(response?.data?.data)
             setUserData(response?.data?.data)
+            setTotal(response?.data?.total_count)
+        })
+    }
+    const fetchRoleData = () => {
+        axios({
+            method: 'get',
+            url: `${base_url}/users/roles/`,
+            headers: {
+                "Authorization": "Bearer " + token
+            }
+        }).then((response) => {
+            var data = response?.data?.data
+            var arr = []
+            //eslint-disable-next-line
+            data.map(it => {
+                if (it?.name === "Regional Admin" || it?.name === "Regional Team Member") {
+                    arr.push(it)
+                }
+            })
+            setRoleData(arr)
         })
     }
     useEffect(() => {
         fetchUserListData()
-    }, [])
-    const handleActiveStatus = (event) => {
-
+        // eslint-disable-next-line
+    }, [skip,limit])
+    const handleActiveStatus = (event, row) => {
+        axios({
+            method: 'put',
+            url: `${base_url}/users/${row?.id}`,
+            headers: {
+                "Authorization": "Bearer " + token
+            },
+            data: {
+                "name": row?.name,
+                "username": row?.username,
+                "role_id": Number(row?.role_id),
+                "active": event.target.checked
+            }
+        }).then((response) => {
+            let data = response?.data
+            if (data?.status) {
+                fetchUserListData()
+                Swal.fire({
+                    toast: true,
+                    position: "center",
+                    icon: "success",
+                    title: "User updated successfully",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            }
+            else {
+                Swal.fire({
+                    toast: true,
+                    icon: "error",
+                    title: "Oops...",
+                    text: data?.message,
+                });
+            }
+        }).catch((error) => {
+            console.log(error)
+        })
     }
     const onSubmit = (data) => {
-        console.log(data);
+        var postData;
+        if (title === "Add User") {
+            postData = {
+                method: 'post',
+                url: `${base_url}/users/register/`,
+                headers: {
+                    "Authorization": "Bearer " + token
+                },
+                data: {
+                    "name": data?.name,
+                    "username": data?.username,
+                    "password": data?.password,
+                    "role_id": Number(data?.role),
+                    "active": true
+                }
+            }
+        }
+        else {
+            postData = {
+                method: 'put',
+                url: `${base_url}/users/${editData?.id}`,
+                headers: {
+                    "Authorization": "Bearer " + token
+                },
+                data: {
+                    "name": data?.name,
+                    "username": data?.username,
+                    "role_id": Number(data?.role)
+                }
+            }
+        }
+        axios(postData).then((response) => {
+            let data = response?.data
+            if (data?.status) {
+                handleCancel()
+                fetchUserListData()
+                document.getElementById("close").click()
+                Swal.fire({
+                    toast: true,
+                    position: "center",
+                    icon: "success",
+                    title: title === "Add User" ? "User added successfully" : "User updated successfully",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            }
+            else {
+                Swal.fire({
+                    toast: true,
+                    icon: "error",
+                    title: "Oops...",
+                    text: data?.message,
+                });
+            }
+        }).catch((error) => {
+            console.log(error)
+        })
     };
     const handleCancel = () => {
-        reset()
+        setTile("Add User")
+        reset({
+            name: "",
+            username: "",
+            password: "",
+            role: "",
+            confirmPassword: ""
+        })
+        setEditData()
+    }
+    const handlePageChange = (page) => {
+        setSkip((page-1) * limit)
+    }
+    const handlePerRowsChange =(newPerPage, page) => {
+        setLimit(newPerPage)
+    }
+    const handleAdd = () => {
+        fetchRoleData()
     }
     return (
         <div>
             <div className='mt-2'>
                 <div className='d-flex justify-content-end'>
-                    <button className='btn btn-success btn-sm' data-bs-toggle="modal" data-bs-target="#staticBackdrop">Add</button>
+                    <button className='btn btn-success btn-sm' data-bs-toggle="modal" data-bs-target="#staticBackdrop" onClick={() => handleAdd()}>Add</button>
                 </div>
                 <DataTable
                     columns={columns}
                     data={userdata}
                     customStyles={customStyles}
                     pagination
+                    paginationServer
+                    paginationTotalRows={total}
+                    onChangeRowsPerPage={handlePerRowsChange}
+                    onChangePage={handlePageChange}
                 />
             </div>
             <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h1 className="modal-title fs-5" id="staticBackdropLabel">Add User</h1>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={() => handleCancel()}></button>
+                            <h1 className="modal-title fs-5" id="staticBackdropLabel">{title}</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" id="close" onClick={() => handleCancel()}></button>
                         </div>
                         <form onSubmit={handleSubmit(onSubmit)} autoComplete='off'>
                             <div className="modal-body">
@@ -124,23 +351,30 @@ function User() {
                                     <input id="username" className='form-control' {...register('username')} autoComplete='off' />
                                     {errors.username && <span className='text-danger'>{errors.username.message}</span>}
                                 </div>
-
-                                <div>
-                                    <label htmlFor="password">Password</label>
-                                    <input id="password" className='form-control' type="password" {...register('password')} autoComplete='off' />
-                                    {errors.password && <span className='text-danger'>{errors.password.message}</span>}
-                                </div>
-                                <div>
-                                    <label htmlFor="confirmPassword">Confirm Password</label>
-                                    <input id="confirmPassword" className='form-control' type="password" {...register('confirmPassword')} />
-                                    {errors.confirmPassword && <span className='text-danger'>{errors.confirmPassword.message}</span>}
-                                </div>
+                                {
+                                    title === "Edit User" ? "" :
+                                        <>
+                                            <div>
+                                                <label htmlFor="password">Password</label>
+                                                <input id="password" className='form-control' type="text" {...register('password')} autoComplete='off' />
+                                                {errors.password && <span className='text-danger'>{errors.password.message}</span>}
+                                            </div>
+                                            <div>
+                                                <label htmlFor="confirmPassword">Confirm Password</label>
+                                                <input id="confirmPassword" className='form-control' type="text" {...register('confirmPassword')} />
+                                                {errors.confirmPassword && <span className='text-danger'>{errors.confirmPassword.message}</span>}
+                                            </div>
+                                        </>
+                                }
                                 <div>
                                     <label htmlFor="role">Role</label>
                                     <select className='form-control' id="role" {...register('role')}>
                                         <option value="">Select Role</option>
-                                        <option value="Region Admin">Region Admin</option>
-                                        <option value="Region Team Member">Region Team Member</option>
+                                        {
+                                            roleData?.map((item, index) => {
+                                                return <option key={index} value={item?.id}>{item?.name}</option>
+                                            })
+                                        }
                                     </select>
                                     {errors.role && <span className='text-danger'>{errors.role.message}</span>}
                                 </div>
@@ -148,7 +382,7 @@ function User() {
                             </div>
                             <div className="modal-footer">
                                 <button type="button" className="btn btn-secondary btn-sm" data-bs-dismiss="modal" title='Cancel' onClick={() => handleCancel()}>Cancel</button>
-                                <button type="submit" className="btn btn-success btn-sm">Save</button>
+                                <button type="submit" className="btn btn-success btn-sm">{title === "Add User" ? "Save" : "Update"}</button>
                             </div>
                         </form>
                     </div>
